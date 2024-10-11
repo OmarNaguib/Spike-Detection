3114bc8092f5ee5e71adb2ac5a30c97d1250ba9f
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-10-11 04:57:49 +0300

    Amend the very beautiful app enhancing its much appreciated beauty and grace

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,44 +1,32 @@
-import React, { useEffect, useRef, useState } from "react";
-import ReactECharts from "echarts-for-react";
+// src/components/Chart.jsx
+/* eslint-disable react/prop-types */
+import ReactEcharts from "echarts-for-react";
 
 const Chart = ({ data, spikes }) => {
-  const [options, setOptions] = useState({});
-  const prevSpikesRef = useRef([]);
-
-  useEffect(() => {
-    const prevSpikes = prevSpikesRef.current;
-    const spikesChanged = JSON.stringify(prevSpikes) !== JSON.stringify(spikes);
-
-    if (spikesChanged) {
-      setOptions({
-        xAxis: {
-          type: "category",
-          data: data.map((_, index) => index),
+  const option = {
+    xAxis: {
+      type: "category",
+      data: data.map((_, index) => index),
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: data,
+        type: "line",
+        markPoint: {
+          data: spikes.map((index) => ({
+            xAxis: index,
+            yAxis: data[index],
+            name: "Spike",
+          })),
         },
-        yAxis: {
-          type: "value",
-        },
-        series: [
-          {
-            data: data,
-            type: "line",
-            markPoint: {
-              data: spikes.map((index) => ({
-                xAxis: index,
-                yAxis: data[index],
-                name: "Spike",
-              })),
-            },
-          },
-        ],
-      });
+      },
+    ],
+  };
 
-      // Update the previous spikes reference
-      prevSpikesRef.current = spikes;
-    }
-  }, [data, spikes]);
-
-  return <ReactECharts option={options} />;
+  return <ReactEcharts option={option} />;
 };
 
 export default Chart;
