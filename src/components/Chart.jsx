eee702fdfd880f0ecd7ac11ffceab6080355e6c8
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-11-15 19:18:42 +0200

    Add some chart options

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,6 +2,11 @@
 /* eslint-disable react/prop-types */
 import ReactEcharts from "echarts-for-react";
 
+const formatNumber = (num, notation, locale = "en") => {
+  const formatter = Intl.NumberFormat(locale, { notation: notation });
+  return formatter.format(num);
+};
+
 const Chart = ({ data, spikes }) => {
   const option = {
     xAxis: {
@@ -11,9 +16,11 @@ const Chart = ({ data, spikes }) => {
     yAxis: {
       type: "value",
     },
+    // label: { show: true },
     series: [
       {
         data: data,
+        // smooth: true,
         type: "line",
         markPoint: {
           data: spikes.map((index) => ({
@@ -22,6 +29,20 @@ const Chart = ({ data, spikes }) => {
             name: "Spike",
           })),
         },
+        // label: {
+        //   show: true,
+        //   formatter: function (params) {
+        //     return formatNumber(params.value, "compact", "en");
+        //   },
+        //   padding: 10,
+        //   fontSize: 14,
+        //   lineHeight: 19,
+        //   distance: 20,
+        //   borderWidth: 1,
+        //   borderType: "solid",
+        //   position: "bottom",
+        //   formatter: "{c}",
+        // },
       },
     ],
   };
